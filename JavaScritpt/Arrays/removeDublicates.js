6d81e962926c79c13253ad73b837cec68af2e56e
NAVEEN KUMAR <naveen.singh@oakum.in>
2024-10-13 11:31:02 +0530

    dublicate element has been removed

--- JavaScritpt/Arrays/removeDublicates.js
+++ JavaScritpt/Arrays/removeDublicates.js
@@ -1,5 +1,4 @@
 var removeElement = function(nums, val) {
-  
     let n=nums.length;
     let j=0
     for(let i=0;i<n;i++){
@@ -9,4 +8,17 @@ var removeElement = function(nums, val) {
     }
     return j;
 };
+
+var removeDuplicates = function(nums) {
+    let j=0;
+    let n=nums.length;
+    for(let i=0;i<n;i++){
+        if(nums[i] !=nums[i+1]){
+            nums[j++]=nums[i]
+        }
+    }
+    return j
+    
+};
+
 console.log("removeElement", removeElement([3,2,2,3,4],3));
