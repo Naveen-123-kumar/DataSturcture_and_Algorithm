f6a2617d6574efe77f7b4256faf6d443eb2eb78b
NAVEEN KUMAR <naveen.singh@oakum.in>
2024-10-17 22:34:44 +0530

    pascal triangle part 2 has been done

--- JavaScritpt/Arrays/pascalTriangle.js
+++ JavaScritpt/Arrays/pascalTriangle.js
@@ -10,4 +10,17 @@ var generate = function (numRows) {
   console.log(result);
 };
 
-generate(5);
+// generate(5);
+
+var getRow = function (rowIndex) {
+  let result = []
+  for (let i = 0; i <= rowIndex; i++) {
+      let arr = new Array(i + 1).fill(1);
+      for (let j = 1; j < i; j++) {
+          arr[j]=result[i-1][j]+result[i-1][j-1]
+      }
+      result.push(arr)
+  }
+  return result[rowIndex];
+};
+console.log(getRow(3))
